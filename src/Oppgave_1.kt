d2b822dce34b7399bfae3442b1114362820e4363
Inge <inge.wang@knowit.no>
2020-08-28 12:16:21 +0200

    Endret tilbake til original

--- src/Oppgave_1.kt
+++ src/Oppgave_1.kt
@@ -2,9 +2,9 @@
 // Feil kan fikses ved å endre datatype eller verdi.
 // Det er lov å legge til nye variabler
 fun main() {
-    var x: Int = 65  // Hva er feil?
-    var isPunk: Boolean = true  // Hvilken type har isPunk?
-    var message = "Hello" // Hva er feil?
+    var x: Int = 65.2  // Hva er feil?
+    var isPunk = true  // Hvilken type har isPunk?
+    var message = 'Hello' // Hva er feil?
     var y = 7 // Hvilken type har y?
     val z: Int = y // Hvilken verdi har z?
 
@@ -16,8 +16,8 @@ fun main() {
     val smallNum = b.toShort() // Hvilken type har smallNum
     val k = y.toDouble() // Hvilken type har k?
 
-    val g = k.toInt().toByte() // Hva er feil?
-    val f = smallNum // Hva er feil?
-    isPunk = false // Hva er feil?
+    b = k.toByte() // Hva er feil?
+    b = smallNum // Hva er feil?
+    isPunk = "false" // Hva er feil?
     s = 0b10001 // Hvilken verdi har s?
 }
